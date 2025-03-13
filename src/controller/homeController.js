58628497084334702b9d28ed71baab159f4d786c
DucQuyen3718 <phamduc3718@gmail.com>
2025-03-14 01:34:26 +0700

    Create Nav/ EJS LOOP

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,6 +1,26 @@
+import connection from "../configs/connectDB"
+
 let getHomePage = (req, res) => {
     //logic 
-    return res.render('../views/index.ejs')
+    let data = []
+    connection.query(
+        'SELECT * FROM `users`',
+        function (err, results, fields) {
+            // console.log('>>> check mysql')
+            // console.log(results); // results contains rows returned by server
+            results.map((row) => {
+                data.push({
+                    id: row.id,
+                    email: row.email,
+                    address: row.address,
+                    firstName: row.firstName,
+                    lastName: row.lastName
+                })
+            })
+            return res.render('../views/index.ejs', { dataUser: data })
+
+        }
+    );
 }
 
 module.exports = {
