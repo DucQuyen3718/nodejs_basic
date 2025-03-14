fcdb022786f4b1706175a68a71f05203399c6b37
DucQuyen3718 <phamduc3718@gmail.com>
2025-03-14 14:36:47 +0700

    RESTFull API

---
+++ src/controller/APIController.js
@@ -0,0 +1,79 @@
+import pool from "../configs/connectDB"
+
+let getAllUsers = async (req, res) => {
+    //http status (search google)
+
+    // 2 chuẩn format là json/xml => json phổ biến hơn (dùng json)
+    // json => object
+
+    const [rows, fields] = await pool.execute('SELECT * FROM users');
+
+    return res.status(200).json({
+        message: 'ok',
+        data: rows
+    })
+}
+
+
+let createNewUser = async (req, res) => {
+    let { firstName, lastName, email, address } = req.body;
+
+    if (!firstName || !lastName || !email || !address) {
+        return res.status(500).json({
+            message: 'missing required params'
+        })
+    }
+    await pool.execute(
+        `INSERT INTO users(firstName, lastName, email, address) values(?, ?, ?, ?)`,
+        [firstName, lastName, email, address]
+    )
+
+    return res.status(200).json({
+        message: 'ok'
+    })
+}
+let updateUser = async (req, res) => {
+    let { firstName, lastName, email, address, id } = req.body;
+
+
+    if (!id || !firstName || !lastName || !email || !address) {
+        return res.status(500).json({
+            message: 'missing required params'
+        })
+    }
+
+    await pool.execute(
+        `UPDATE users SET firstName = ?, lastName = ?, email = ?, address = ? WHERE id = ?`,
+        [firstName, lastName, email, address, id])
+
+    return res.status(200).json({
+        message: 'ok'
+    })
+}
+
+let deleteUser = async (req, res) => {
+    let userId = req.params.id;
+    if (!userId) {
+        return res.status(500).json({
+            message: 'missing required params'
+        })
+    }
+
+    await pool.execute(
+        `DELETE FROM users where id = ?`,
+        [userId]
+    )
+
+    return res.status(200).json({
+        message: 'ok'
+    })
+}
+
+
+
+module.exports = {
+    getAllUsers,
+    createNewUser,
+    updateUser,
+    deleteUser
+}
