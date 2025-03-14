2a3e54d319177e215a93b3ebc3fc00e52711f209
DucQuyen3718 <phamduc3718@gmail.com>
2025-03-14 17:02:26 +0700

    Upload Single File

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,6 @@
 import pool from "../configs/connectDB"
+import multer from "multer";
+
 
 let getHomePage = async (req, res) => {
 
@@ -51,11 +53,52 @@ let postUpdateUser = async (req, res) => {
 
     return res.redirect('/')
 }
+
+let getUploadFilePage = async (req, res) => {
+    return res.render('../views/uploadFile.ejs')
+}
+
+const upload = multer().single(`profile_pic`)
+
+
+
+
+// exports.imageFilter = imageFilter;
+
+let handleUploadFile = async (req, res) => {
+    // 'profile_pic' is the name of our file input field in the HTML form
+    //let upload = multer({ storage: storage, fileFilter: imageFilter }).single('profile_pic');
+    // console.log(req.file)
+    upload(req, res, function (err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="/images/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
+    });
+}
+
+
 module.exports = {
     getHomePage,
     getDetailPage,
     createNewUser,
     deleteUser,
     getEditUser,
-    postUpdateUser
+    postUpdateUser,
+    getUploadFilePage,
+    handleUploadFile
 }
